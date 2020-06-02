9643b2803346399a5daa4b829bc16e2efce95567
seanyu4296 <seanyu4296@gmail.com>
2020-06-02 15:37:12 +0000

    Add hmr

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom";
+import React from "react";
 import { main } from "./output/Sample.Main";
 
 const Main = main();
 
 ReactDOM.render(<Main />, document.getElementById("app"));
+
+if (module.hot) {
+  module.hot.accept();
+}
